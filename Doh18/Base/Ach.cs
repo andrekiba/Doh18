020baaef7f34161a1e5377f51c035ac02e73e2f3
Andrea <andre.kiba@gmail.com>
2018-10-19 08:04:56 +0200

    update

--- Doh18/Base/Ach.cs
+++ Doh18/Base/Ach.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Doh18.Helpers;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
@@ -7,24 +8,37 @@ using Plugin.Connectivity;
 
 namespace Doh18.Base
 {
-    public static class Ach //AppCenterHelper
+    public static class Ach
     {
-        public static void TrackError(Exception ex, IDictionary<string, string> properties = null)
+        //public static void TrackError(Exception ex, IDictionary<string, string> properties = null)
+        //{
+        //    var baseProps = new Dictionary<string, string>
+        //    {
+        //        {"Email", Settings.Current.Email},
+        //        {"Connected", CrossConnectivity.Current.IsConnected.ToString()}
+        //    };
+
+        //    Crashes.TrackError(ex, baseProps.Merge(properties));
+        //}
+
+        public static void TrackError(this Exception ex, IDictionary<string, string> properties = null, [CallerMemberName]string caller = "")
         {
             var baseProps = new Dictionary<string, string>
             {
                 {"Email", Settings.Current.Email},
+                {Constants.Where, caller},
                 {"Connected", CrossConnectivity.Current.IsConnected.ToString()}
             };
 
             Crashes.TrackError(ex, baseProps.Merge(properties));
         }
 
-        public static void TrackEvent(string key, IDictionary<string, string> properties = null)
+        public static void TrackEvent(this string key, IDictionary<string, string> properties = null, [CallerMemberName]string caller = "")
         {
             var baseProps = new Dictionary<string, string>
             {
                 {"Email", Settings.Current.Email},
+                {Constants.Where, caller},
                 {"Connected", CrossConnectivity.Current.IsConnected.ToString()}
             };
 
--- Doh18/ViewModels/MainViewModel.cs
+++ Doh18/ViewModels/MainViewModel.cs
@@ -1,10 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Acr.UserDialogs;
+using Doh18.Base;
+using Xamarin.Forms;
 
 namespace Doh18.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        #region Lifecycle
+        public MainViewModel()
+        {
+            
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand sayCiaoCommand;
+        public ICommand SayCiaoCommand => sayCiaoCommand ?? (sayCiaoCommand = new Command(async () =>
+        {
+            await UserDialogs.Instance.AlertAsync("Ciao!", null, "OK");
+        }));
+
+        private ICommand trackEventCommand;
+        public ICommand TrackEventCommand => trackEventCommand ?? (trackEventCommand = new Command(() =>
+        {
+            "Button clicked!".TrackEvent();
+        }));
+
+        private ICommand handleCommand;
+        public ICommand HandleCommand => handleCommand ?? (handleCommand = new Command(() =>
+        {
+            try
+            {
+                throw new Exception("Handled exception");
+            }
+            catch (Exception e)
+            {
+                e.TrackError();
+            }
+        }));
+
+        private ICommand crashCommand;
+        public ICommand CrashCommand => crashCommand ?? (crashCommand = new Command(() => throw new Exception("UnHandled exception")));
+
+        #endregion
     }
 }
